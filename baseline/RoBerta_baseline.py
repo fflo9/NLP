d305973cf6004f3a6e829df4f6ec316184b08e67
Sara <saracetin889@gmail.com>
2026-05-01 11:18:28 +0200

    cleaned baseline

--- baseline/RoBerta_baseline.py
+++ baseline/RoBerta_baseline.py
@@ -42,58 +42,49 @@ def read_file(path):
 
      return data
 
-train_data = read_file(r'baseline/en_ewt-ud-train.iob2')
-dev_data = read_file(r'baseline/en_ewt-ud-dev.iob2')
+def dictionize(train_data, dev_data, test_data, label2id):
+    train_dict = {
+        'tokens': [],
+        'ner_tags': []
+    }
+    #loop through the train data
+    for words, tags in train_data:
+        #add the raw str words to the tokens
+        train_dict['tokens'].append(words)
 
-id2label = {
-    0: "O",
-    1: "B-LOC",
-    2: "I-LOC",
-    3: "B-PER",
-    4: "I-PER",
-    5: "B-ORG",
-    6: "I-ORG",
-}
-label2id = {
-    "O": 0,
-    "B-LOC": 1,
-    "I-LOC": 2,
-    "B-PER": 3,
-    "I-PER": 4,
-    "B-ORG": 5,
-    "I-ORG": 6,
-}
+        #convert the string tags to ID numbers using label2id map
+        tag_ids = [label2id[tag] for tag in tags]
+        train_dict['ner_tags'].append(tag_ids)
 
-train_dict = {
-    'tokens': [],
-    'ner_tags': []
-}
+    #dev data
+    dev_dict = {
+        'tokens': [],
+        'ner_tags': []
+    }
+    for words, tags in dev_data:
+        dev_dict['tokens'].append(words)
+        tag_ids = [label2id[tag] for tag in tags]
+        dev_dict['ner_tags'].append(tag_ids)
 
-#loop through the train data
-for words, tags in train_data:
-    #add the raw str words to the tokens
-    train_dict['tokens'].append(words)
+    #test data
+    test_dict = {
+        'tokens': [],
+        'ner_tags': []
+    }
+    for words, tags in test_data:
+        test_dict['tokens'].append(words)
+        test_dict['ner_tags'].append([label2id[tag] for tag in tags])
 
-    #convert the string tags to ID numbers using label2id map
-    tag_ids = [label2id[tag] for tag in tags]
-    train_dict['ner_tags'].append(tag_ids)
+    #convert all 3 dictionaries to a hf dataset then datasetdict
+    trainDataset = Dataset.from_dict(train_dict)
+    devDataset = Dataset.from_dict(dev_dict)
+    testDataset = Dataset.from_dict(test_dict)
+    datasetDict = DatasetDict({'train': trainDataset, 'dev': devDataset, 'test': testDataset})
 
-#the same for dev data
-dev_dict = {
-    'tokens': [],
-    'ner_tags': []
-}
-
-for words, tags in dev_data:
-    dev_dict['tokens'].append(words)
-    tag_ids = [label2id[tag] for tag in tags]
-    dev_dict['ner_tags'].append(tag_ids)
-
-#set the tokenizer 
-tokenizer = RobertaTokenizerFast.from_pretrained('FacebookAI/roberta-base', add_prefix_space = True)
+    return datasetDict
 
 #aligning the labels
-def tokenize_and_align_labels(examples):
+def tokenize_and_align_labels(tokenizer, examples):
     tokenized_inputs = tokenizer(examples['tokens'], truncation=True, is_split_into_words=True)
 
     labels = []
@@ -114,29 +105,7 @@ def tokenize_and_align_labels(examples):
     tokenized_inputs['labels'] = labels
     return tokenized_inputs
 
-#convert the dictionaries to a hf dataset then datasetdict
-trainDataset = Dataset.from_dict(train_dict)
-devDataset = Dataset.from_dict(dev_dict)
-
-datasetDict = DatasetDict({'train': trainDataset, 'dev': devDataset})
-tokenizedData = datasetDict.map(tokenize_and_align_labels, batched=True)
-print('Columns in the new dataset:', tokenizedData['train'].column_names)
-
-#example result
-print(tokenizedData['train'][1])
-print(tokenizedData['dev'][5])
-
-#automatic padding, and converts data to tensors
-data_collator = DataCollatorForTokenClassification(tokenizer)
-
-model = RobertaForTokenClassification.from_pretrained(
-   'FacebookAI/roberta-base', num_labels=7, id2label=id2label, label2id=label2id
-)
-
-train_final = tokenizedData['train'].remove_columns(['tokens', 'ner_tags'])
-dev_final = tokenizedData['dev'].remove_columns(['tokens', 'ner_tags'])
-
-#initialize trainer with weighted classes
+#trainer with weighted classes
 class WeightedTrainer(Trainer):
   def compute_loss(self, model, inputs, return_outputs=False, **kwargs):
     labels = inputs.get('labels')
@@ -145,12 +114,99 @@ class WeightedTrainer(Trainer):
     outputs = model(**inputs)
     logits = outputs.get('logits')
 
-    weights = torch.tensor([0.5, 2.0, 2.0, 1.2, 1.2, 2.5, 2.5])
+    weights = torch.tensor([0.5, 2.0, 2.0, 1.2, 1.2, 2.5, 2.5]).to(logits.device)
 
     loss_fct = torch.nn.CrossEntropyLoss(weight=weights)
     loss = loss_fct(logits.view(-1, self.model.config.num_labels), labels.view(-1))
 
     return (loss, outputs) if return_outputs else loss
+
+#get the classification report
+def compute_metrics(predictions, labels, label_list):
+
+    #remove -100 (padding) and convert id2label
+    true_predictions = [
+        [label_list[p] for (p, l) in zip(prediction, label) if l != -100]
+        for prediction, label in zip(predictions, labels)
+    ]
+    true_labels = [
+        [label_list[l] for (p, l) in zip(prediction, label) if l != -100]
+        for prediction, label in zip(predictions, labels)
+    ]
+
+    return classification_report(true_labels, true_predictions)
+
+def predictions(trainer, data, label_list):
+    #get predictions
+    output = trainer.predict(data)
+    predictions = np.argmax(output.predictions, axis=2) #the label w/ the highest score
+    
+    #ground truth
+    labels = output.label_ids
+
+    return predictions, labels
+
+#save the results
+def save_results(file_path, data, pred_array, tokenized_data, label_list):
+    with open(f'{file_path}.iob2', 'w', encoding='utf-8') as file:
+        for i, sentence_data in enumerate(data):
+            original_words = sentence_data[0]
+            word_ids = tokenized_data[i]['input_ids']
+            preds = pred_array[i]
+
+            # Align subwords
+            word_predictions = []
+            previous_word_idx = None
+            for j, word_idx in enumerate(word_ids):
+                if word_idx is None: continue
+                if word_idx != previous_word_idx:
+                    word_predictions.append(label_list[preds[j]])
+                previous_word_idx = word_idx
+
+            # 3. Write using \t (Tabs)
+            for idx, (word, pred_tag) in enumerate(zip(original_words, word_predictions)):
+                # Force tab separation
+                file.write(f'{idx}\t{word}\t{pred_tag}\n')
+
+            # Sentence break
+            file.write('\n')
+
+    print('File saved successfully')
+
+#load train and dev data
+train_data = read_file(r'baseline/en_ewt-ud-train.iob2')
+dev_data = read_file(r'baseline/en_ewt-ud-dev.iob2')
+test_data = read_file(r'baseline/en_ewt-ud-test.iob2')
+
+id2label = {
+    0: "O",
+    1: "B-LOC",
+    2: "I-LOC",
+    3: "B-PER",
+    4: "I-PER",
+    5: "B-ORG",
+    6: "I-ORG",
+}
+label2id = {
+    "O": 0,
+    "B-LOC": 1,
+    "I-LOC": 2,
+    "B-PER": 3,
+    "I-PER": 4,
+    "B-ORG": 5,
+    "I-ORG": 6,
+}
+
+#set the tokenizer 
+tokenizer = RobertaTokenizerFast.from_pretrained('FacebookAI/roberta-base', add_prefix_space = True)
+
+#automatic padding, and converts data to tensors
+data_collator = DataCollatorForTokenClassification(tokenizer)
+
+#initialize model
+model = RobertaForTokenClassification.from_pretrained(
+   'FacebookAI/roberta-base', num_labels=7, id2label=id2label, label2id=label2id
+)
 
 #initialize training arguements
 training_args = TrainingArguments(
@@ -168,6 +224,19 @@ training_args = TrainingArguments(
     weight_decay=0.01,
 )
 
+#tokenize all splits
+datasetDict = dictionize(train_data, dev_data, test_data, label2id)
+tokenizedData = datasetDict.map(
+    lambda examples: tokenize_and_align_labels(tokenizer, examples), 
+    batched=True
+)
+
+#remove raw columns for all splits
+train_final = tokenizedData['train'].remove_columns(['tokens', 'ner_tags'])
+dev_final = tokenizedData['dev'].remove_columns(['tokens', 'ner_tags'])
+test_final = tokenizedData['test'].remove_columns(['tokens', 'ner_tags'])
+
+#set trainer and start training
 trainer = WeightedTrainer(
     model=model,
     args=training_args,
@@ -176,56 +245,15 @@ trainer = WeightedTrainer(
     data_collator=data_collator,
 )
 
-#start training
 trainer.train()
 
-#get dev predictions
-output = trainer.predict(dev_final)
-predictions = np.argmax(output.predictions, axis=2) #the label w/ the highest score
-#ground truth
-labels = output.label_ids
 #the IOB tags
 label_list = ['O', 'B-LOC', 'I-LOC', 'B-PER', 'I-PER', 'B-ORG', 'I-ORG']
 
-#get the classification report
-def compute_metrics(output, predictions):
+#predictions and classification report
+pred_array, labels = predictions(trainer, test_final, label_list)
+class_report = compute_metrics(pred_array, labels, label_list)
+print(class_report)
 
-    #remove -100 (padding) and convert id2label
-    true_predictions = [
-        [label_list[p] for (p, l) in zip(prediction, label) if l != -100]
-        for prediction, label in zip(predictions, labels)
-    ]
-    true_labels = [
-        [label_list[l] for (p, l) in zip(prediction, label) if l != -100]
-        for prediction, label in zip(predictions, labels)
-    ]
-
-    return classification_report(true_labels, true_predictions)
-
-print(compute_metrics(output, predictions))
-
-#save the results
-with open('/content/roberta_predictions.iob2', 'w', encoding='utf-8') as file:
-    for i, sentence_data in enumerate(dev_data):
-        original_words = sentence_data[0]
-        word_ids = tokenizedData['dev'][i]['input_ids']
-        preds = predictions[i]
-
-        # Align subwords
-        word_predictions = []
-        previous_word_idx = None
-        for j, word_idx in enumerate(word_ids):
-            if word_idx is None: continue
-            if word_idx != previous_word_idx:
-                word_predictions.append(label_list[preds[j]])
-            previous_word_idx = word_idx
-
-        # 3. Write using \t (Tabs)
-        for idx, (word, pred_tag) in enumerate(zip(original_words, word_predictions)):
-            # Force tab separation
-            file.write(f'{idx}\t{word}\t{pred_tag}\n')
-
-        # Sentence break
-        file.write('\n')
-
-print('File saved successfully')
+#save results
+#save_results('test_predictions', test_data, pred_array, tokenizedData['test'], label_list)
