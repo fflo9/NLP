f4ed62312af96b02f53ac048477fc2c2b65656e6
Sara <saracetin889@gmail.com>
2026-05-12 10:27:19 +0200

    typo

--- src/__init__.py
+++ src/__init__.py
@@ -2,7 +2,7 @@ from .custom_model import BertCRFForNER, WeightedTrainer
 from .utils import (
     read_file, 
     dictionize, 
-    tokenize_and_salign_labels, 
+    tokenize_and_align_labels, 
     compute_metrics, 
     compute_metrics_model,
     predictions
